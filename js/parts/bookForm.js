294c9d8c22bf4a45a31133ccde6fdfdbc912e270
butterandfly <zero.hero.lin@gmail.com>
2015-01-16 18:52:22 +0800

    更新了表单，添加了活动详情

--- js/parts/bookForm.js
+++ js/parts/bookForm.js
@@ -2,8 +2,13 @@
   "use strict";
 
   var bottomFloat = $('.bottom-float');
+  var formPage = $('.form-page');
   var nameInputElm = $('.form-page input[name=uname]');
   var mobileInputElm = $('.form-page input[name=mobile]');
+  var agreeCheckbox = formPage.find('input[name=agree]');
+
+  var submitBtn = $('.submit-btn');
+
 
   // 输入框事件，隐藏悬浮窗
   nameInputElm.on('focus', function() {
@@ -12,7 +17,6 @@
   nameInputElm.on('blur', function() {
     bottomFloat.show();
   });
-
   mobileInputElm.on('focus', function() {
     bottomFloat.hide();
   });
@@ -20,22 +24,34 @@
     bottomFloat.show();
   });
 
+
+  // 同意提交个人信息
+  agreeCheckbox.on('touchend', function( ) {
+    ___log('点击是否同意checkbox, 点击后的结果为: ' + agreeCheckbox.prop('checked'));
+  });
+
   // 活动详情
   var detailModal = $('.sf-modal');
   var moreDetailBtn = $('.more-detail');
   moreDetailBtn.on('touchend', function() {
     detailModal.removeClass('hide');
+    ___log('显示活动详情');
   });
 
   // 提交按钮事件
   // 发送数据
-  $(".submit-btn").on('touchend', function (ev) {
+  submitBtn.on('touchend', function (ev) {
     ev.preventDefault();
     // 注意，这里使用setTimeout是因为ios上直接调用含alert的方法会导致事件在其他touchend事件触发时的二次触发
     setTimeout(function () {
       var uname = nameInputElm.val();
       var mobile = mobileInputElm.val();
-      //var sex = $('input[name=sex]:checked').val();
+      var agree = agreeCheckbox.prop('checked');
+
+      // 下面3行为测试代码，实际投放时请注释掉
+      //alert(uname);
+      //alert(mobile);
+      //alert(agree);
 
       if (!uname) {
         ___log("名称缺失");
@@ -47,10 +63,10 @@
         return alert("请填写电话");
       }
 
-      // 下面3行为测试代码，实际投放时请注释掉
-//        alert(uname);
-//        alert(mobile);
-//        alert(sex);
+      if (!agree) {
+        ___log('不同意提交');
+        return alert('你需要同意提交个人信息');
+      }
 
       _CWiQ.push(['_trackReg', 1]);
       ___log('submit');
@@ -60,7 +76,7 @@
         labels: [
           {'1': uname}
           , {'2': mobile}
-          , {'3': window.sf_UserIp}
+          , {'3': window._shunfei.userIp}
           , {'4': ''}
           , {'5': ''}
           , {'6': ''}
--- js/parts/modal.js
+++ js/parts/modal.js
@@ -3,9 +3,15 @@
 
   var modal = $('.sf-modal');
   var modalCloseBtn = $('.sf-modal-close');
+  var modalBackdrop = $('.sf-modal-backdrop');
 
   var tapend = _shunfei.TAPEND || 'touchend';
   modalCloseBtn.on(tapend, function() {
     modal.addClass('hide');
+    ___log('关闭详情页');
   })
+  modalBackdrop.on(tapend, function() {
+    modal.addClass('hide');
+    ___log('关闭详情页');
+  });
 })();
